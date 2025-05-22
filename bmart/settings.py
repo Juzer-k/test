da80c34813f4425759891e4482e14f824dcd4769
Juzer-k <juzerkathiyawadi53@gmail.com>
2025-05-22 20:34:35 +0530

    dddd

--- bmart/settings.py
+++ bmart/settings.py
@@ -22,7 +22,7 @@ if DEBUG:
     environ.Env.read_env()
 
 SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']  # Add your Render URL later
+ALLOWED_HOSTS = ['bmart-f6tq.onrender.com', 'localhost']  # Add your Render URL later
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bmart.urls'
@@ -129,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
